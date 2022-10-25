d1f401b6ac8353fb3df66655cd2ad582dc4dc6da
felix2000jp <felix2000jp@gmail.com>
2022-10-25 13:08:40 +0100

    added user.ID type to req

--- src/Types/fastify-jwt.d.ts
+++ src/Types/fastify-jwt.d.ts
@@ -3,6 +3,7 @@ import "@fastify/jwt";
 declare module "@fastify/jwt" {
   interface FastifyJWT {
     payload: { ID: string };
+    user: { ID: string };
   }
 
   // For now we need this to solve the missing onlyCookie option
