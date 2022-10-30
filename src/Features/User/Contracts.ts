aa447182ae30e89b53419b234a97f2c9a60fa1d5
zebarros17 <61798839+zebarros17@users.noreply.github.com>
2022-10-30 16:08:42 +0000

    Feito mas pouco otimizado

--- src/Features/User/Contracts.ts
+++ src/Features/User/Contracts.ts
@@ -24,3 +24,18 @@ export const deleteUserResponse = Type.Object({
 });
 
 // patch user
+export const patchUserRequest = Type.Object({
+  Email: Type.Optional(Type.String()),
+  Name: Type.Optional(Type.String()),
+  Description: Type.Optional(Type.String()),
+  Social: Type.Optional(Type.String()),
+  Activities: Type.Optional(Type.Array(Type.String())),
+});
+
+export const patchUserResponse = Type.Object({
+  Email: Type.String({ format: "email" }),
+  Name: Type.String(),
+  Description: Type.String(),
+  Social: Type.String(),
+  Activities: Type.Array(Type.String()),
+});
--- src/Features/User/Routes.ts
+++ src/Features/User/Routes.ts
@@ -1,22 +1,31 @@
 import { FastifyInstance } from "fastify";
-import { getUserDetailsResponse, getUserResponse, deleteUserResponse } from "./Contracts";
-import { getUserDetailsController, getUserController, deleteUserController } from "./Controllers";
+import { getUserDetailsResponse, getUserResponse, deleteUserResponse, patchUserResponse, patchUserRequest } from "./Contracts";
+import { getUserDetailsController, getUserController, deleteUserController, patchUserController } from "./Controllers";
 
 const userRoutes = async (app: FastifyInstance) => {
   // get
-  app.get("/", { preHandler: app.auth([app.verifyAccessJWT]), schema: { response: { 200: getUserResponse } } }, getUserController);
+  app.get("/", { preHandler: app.auth([app.verifyAccessJWT]) as any, schema: { response: { 200: getUserResponse } } }, getUserController);
 
   // get details
   app.get(
     "/details",
-    { preHandler: app.auth([app.verifyAccessJWT]), schema: { response: { 200: getUserDetailsResponse } } },
+    { preHandler: app.auth([app.verifyAccessJWT]) as any, schema: { response: { 200: getUserDetailsResponse } } },
     getUserDetailsController
   );
 
   // delete
-  app.delete("/", { preHandler: app.auth([app.verifyAccessJWT]), schema: { response: { 200: deleteUserResponse } } }, deleteUserController);
+  app.delete(
+    "/",
+    { preHandler: app.auth([app.verifyAccessJWT]) as any, schema: { response: { 200: deleteUserResponse } } },
+    deleteUserController
+  );
 
   // patch
+  app.patch(
+    "/",
+    { preHandler: app.auth([app.verifyAccessJWT]) as any, schema: { body: patchUserRequest, response: { 200: patchUserResponse } } },
+    patchUserController
+  );
 };
 
 export default userRoutes;
