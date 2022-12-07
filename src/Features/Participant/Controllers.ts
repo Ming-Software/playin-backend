b1dabdca523b9acd8627b78934678cb8b1f5ccf0
felix2000jp <felix2000jp@gmail.com>
2022-12-07 15:18:10 +0000

    fixed remove participant error message

--- src/Features/Participant/Controllers.ts
+++ src/Features/Participant/Controllers.ts
@@ -35,6 +35,6 @@ export const removeParticipantByOwnerController = async (
 
 		return res.status(200).send({});
 	} catch (error) {
-		return res.status(500).send(error);
+		return res.status(500).send({ ErrorMessage: (error as Error).message });
 	}
 };
